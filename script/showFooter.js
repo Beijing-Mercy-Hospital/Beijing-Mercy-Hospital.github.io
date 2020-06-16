d0c1738523176bafbb30a62656fed432189fdcfd
Beijing_Mercy_Hospital <65579702+a-stranger-from-outerspace@users.noreply.github.com>
2020-06-16 15:32:04 +0800

    Update showFooter.js

--- script/showFooter.js
+++ script/showFooter.js
@@ -2,3 +2,5 @@ var version = "beta 1.0.2"
 var years = "2020"
 var date = "Jun 16th,2020"
 document.write('<p style="color:white;">Beijing Mercy Hospital Powered By OIstreamBlog, witch is designed by Oistream and ASFOS.</p>')
+document.write('<p style="font-family:Consolas;color:white;">Beijing Mercy Hospital Powered By OIstreamBlog ' + version + ' | Designed by (©) ' + years + ' OIstream & ASFOS | 更新于 ' + date + ' | <a href="malito:zxp1863@163.com" style="color:white;">联系我们</a>（zxp1863@163.com）' + ' | <a href="https://beijing-mercy-hospital.github.io/new.txt" style="color:white;">更新记录</a></p>')
+
