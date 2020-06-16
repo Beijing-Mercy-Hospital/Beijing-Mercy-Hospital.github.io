e4cf8ce3ac956009e3bfea8b4f9c8c7ed8d40d27
Beijing_Mercy_Hospital <65579702+a-stranger-from-outerspace@users.noreply.github.com>
2020-06-16 15:29:50 +0800

    Update showFooter.js

--- script/showFooter.js
+++ script/showFooter.js
@@ -1,4 +1,4 @@
-var version = "4.6.2"
+var version = "beta 1.0.2"
 var years = "2020"
-var date = "Jun 13th,2020"
-document.write('<p style="color:white;">Beijing Mercy Hospital Powered By OIstreamBlog.</p>')
+var date = "Jun 16th,2020"
+document.write('<p style="color:white;">Beijing Mercy Hospital Powered By OIstreamBlog, witch is designed by Oistream and ASFOS.</p>')
